05543fa2cbd4cca33dff9e6379ab3a2b84472278
John Atay <johncanaty@gmail.com>
2019-06-05 15:05:08 -0700

    Buttonsandgif

--- app.js
+++ app.js
@@ -1,34 +1,67 @@
-var familyGuy = ["Peter", "Stewie", "Maggie", "Lois", "Chris"];
+var familyGuy = ["Peter", "Chris", "Brain"];
 
-function renderButtons() {
-        
-    // Delete the content inside the buttons-view div prior to adding new movies
-    $( "#buttons-view" ).empty();
-    // (this is necessary otherwise you will have repeat buttons)
+///SUBMIT BTN
+$("#add-person").on("click", function (e) {
+  e.preventDefault();
+  //get the value from input
+  var familyInput = $("#family-input").val().trim();
+  //add a button to id="buttons-view
+  var button = $("<button>")
+    .appendTo("#buttons-view")
+    .text(familyInput)
+    .attr("data-person", familyInput);
 
-    // Loop through the array of charaters, then generate buttons for each person in the array
-    var i;
-    for (i = 0; i < familyGuy.length; i++) { 
-      // $("<div>")
-      // create <button> with jQuery
-        var btn = $("<button>").text(familyGuy[i]);
-         $("#buttons-view").append(btn);
-    } 
-  }
+  //add a text to the button
 
-  $("#add-person").on("click", function(event) {
-    // event.preventDefault() prevents submit button from trying to send a form.
-    // Using a submit button instead of a regular button allows the user to hit
-    // "Enter" instead of clicking the button if desired
-    event.preventDefault();
+});
 
-    // Write code to grab the text the user types into the input field
-    var personInput = $("#family-input").val();
-    console.log(personInput);
-    // Write code to add the new person into the movies array
-    familyGuy.push(personInput);
+// Event listener for all button elements
+$(document).on("click", "button", function () {
+  // In this case, the "this" keyword refers to the button that was clicked
+  var person = $(this).attr("data-person");
 
-    // The renderButtons function is called, rendering the list of movie buttons
-    renderButtons();
-  });
-  renderButtons();
+  // Constructing a URL to search Giphy for the name of the person who said the quote
+  var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
+    person + "&api_key=BkaUZZWcFij6J7AoQj3WtPb1R2p9O6V9&limit=10";
+
+  // Performing our AJAX GET request
+  $.ajax({
+    url: queryURL,
+    method: "GET"
+  })
+    // After the data comes back from the API
+    .then(function (response) {
+      // Storing an array of results in the results variable
+      var results = response.data;
+
+      // Looping over every result item
+      for (var i = 0; i < results.length; i++) {
+
+        // Only taking action if the photo has an appropriate rating
+        if (results[i].rating !== "r" && results[i].rating !== "pg-13") {
+          // Creating a div for the gif
+          var gifDiv = $("<div>");
+
+          // Storing the result item's rating
+          var rating = results[i].rating;
+
+          // Creating a paragraph tag with the result item's rating
+          var p = $("<p>").text("Rating: " + rating);
+
+          // Creating an image tag
+          var personImage = $("<img>");
+
+          // Giving the image tag an src attribute of a proprty pulled off the
+          // result item
+          personImage.attr("src", results[i].images.fixed_height.url);
+
+          // Appending the paragraph and personImage we created to the "gifDiv" div we created
+          gifDiv.append(p);
+          gifDiv.append(personImage);
+
+          // Prepending the gifDiv to the "#gifs-appear-here" div in the HTML
+          $("#gifs-appear-here").prepend(gifDiv);
+        }
+      }
+    });
+});
